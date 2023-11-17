df85489039f66f26f4725ab883afbd002c6d4959
W-Thiago <thipsilva@outlook.com>
2023-11-17 15:17:19 -0300

    adicionando button to JavaScript - 4

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
 import ScrollAnimate from "./modules/scroll-animate.js";
+import MobileMenu from "./modules/mobile-menu.js";
 
 const scrollAnimate = new ScrollAnimate('[date-animate= "scroll"]');
 scrollAnimate.init();
+
+const mobileMenu = new MobileMenu(
+  '[data-menu = "hamburger"]',
+  '[data-menu= "lista"]'
+);
+mobileMenu.init();
